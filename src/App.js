53d43a3d0b0c46a91dfcaf26860c0b1ad17d7c6d
Syed Zafar Abbas <bukhary72pk@yahoo.com>
2020-06-13 14:42:36 +0500

    Created multiple components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import StudentInfo from './StdInfo';
+import Users from './UserInfo';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <StudentInfo stdName="Syed Zafar Abbas Bukhari"
-                   assignNo="1"
-                   stdBatch="3"
-                   stdCenter="Islamabad" />
+    <div>
+        <div className="App">
+          <StudentInfo stdName="Syed Zafar Abbas Bukhari"
+                      assignNo="1"
+                      stdBatch="3"
+                      stdCenter="Islamabad" />
+        </div>
+        
+        <div className="App">
+          <Users username="bukhary72pk" />
+        </div>
     </div>
   );
 }
-
 export default App;
