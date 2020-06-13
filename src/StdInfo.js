4950a3924a530f869c8ecfb6d9390ccc60b14fe3
Syed Zafar Abbas <bukhary72pk@yahoo.com>
2020-06-13 14:43:06 +0500

    Created StdInfo.js component

--- src/StdInfo.js
+++ src/StdInfo.js
@@ -3,11 +3,12 @@ import React from 'react'
 function StudentInfo(props){
     return(
         <div>
+            <h1 id='red'>This is component-1</h1>
             <h1>Assignment No.{props.assignNo}</h1>
-            <hr/>
             <h2>{props.stdName}</h2>
             <h2>PIAIC {props.stdCenter}, Batch-{props.stdBatch}</h2>
-        </div>
+            <hr/>
+        </div>        
     );
 }
 
